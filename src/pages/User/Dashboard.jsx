e18d1102788af6b398b184b91a0b925f060fde5c
Kelvinavq <kvalera200244@gmail.com>
2024-01-17 03:19:42 -0400

    a

--- src/pages/User/Dashboard.jsx
+++ src/pages/User/Dashboard.jsx
@@ -16,64 +16,64 @@ const Dashboard = () => {
   const [showAlert, setShowAlert] = useState(false); 
 
 
-  useEffect(() => {
-    // Verificar si el usuario está autenticado
-    const checkAuthStatus = async () => {
-      try {
-        const response = await fetch(
-          "https://digitalvibra.com/nuovo_backend/backend/Api/check-session.php",
-          {
-            method: "GET",
-            mode: "cors",
-            credentials: "include",
-          }
-        );
+  // useEffect(() => {
+  //   // Verificar si el usuario está autenticado
+  //   const checkAuthStatus = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         "https://digitalvibra.com/nuovo_backend/backend/Api/check-session.php",
+  //         {
+  //           method: "GET",
+  //           mode: "cors",
+  //           credentials: "include",
+  //         }
+  //       );
 
-        const responseData = await response.json();
+  //       const responseData = await response.json();
 
-        if (response.ok) {
-          setIsLoggedIn(true);
+  //       if (response.ok) {
+  //         setIsLoggedIn(true);
 
-          // Verificar el rol del usuario después de la autenticación
-          if (userRole !== "user") {
-            setShowAlert(true);
-            // Si el rol no es user, redirigir al usuario al inicio de sesión
-            Swal.fire({
-              icon: "error",
-              title: "Error",
-              text: "Acceso no permitido para el rol actual.",
-              timer: 3000,
-              didClose: () => {
-                history.back()
-              },
-            });
-          }
-        } else {
-          setShowAlert(true);
-          // Si la sesión no es válida, redirige al usuario al inicio de sesión
-          Swal.fire({
-            icon: "error",
-            title: "Error",
-            text: "Debes iniciar sesión para acceder a esta página.",
-            timer: 3000,
-            didClose: () => {
-              // window.location.href = "/login";
-            },
-          });
-        }
-      } catch (error) {
-        console.error("Error al verificar la sesión:", error);
-      }
-    };
+  //         // Verificar el rol del usuario después de la autenticación
+  //         if (userRole !== "user") {
+  //           setShowAlert(true);
+  //           // Si el rol no es user, redirigir al usuario al inicio de sesión
+  //           Swal.fire({
+  //             icon: "error",
+  //             title: "Error",
+  //             text: "Acceso no permitido para el rol actual.",
+  //             timer: 3000,
+  //             didClose: () => {
+  //               history.back()
+  //             },
+  //           });
+  //         }
+  //       } else {
+  //         setShowAlert(true);
+  //         // Si la sesión no es válida, redirige al usuario al inicio de sesión
+  //         Swal.fire({
+  //           icon: "error",
+  //           title: "Error",
+  //           text: "Debes iniciar sesión para acceder a esta página.",
+  //           timer: 3000,
+  //           didClose: () => {
+  //             // window.location.href = "/login";
+  //           },
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.error("Error al verificar la sesión:", error);
+  //     }
+  //   };
 
-    // Llamar a la función para verificar la sesión
-    checkAuthStatus();
-  }, [history]);
+  //   // Llamar a la función para verificar la sesión
+  //   checkAuthStatus();
+  // }, [history]);
 
-  // Si el usuario no ha iniciado sesión o no tiene el rol adecuado, no renderizar el componente
-  if (!isLoggedIn || showAlert) {
-    return null;
-  }
+  // // Si el usuario no ha iniciado sesión o no tiene el rol adecuado, no renderizar el componente
+  // if (!isLoggedIn || showAlert) {
+  //   return null;
+  // }
 
   return (
     <div className="dashboard">
