bddc92b9c0322dd2f85c6135670db71752d5bb96
Kelvinavq <kvalera200244@gmail.com>
2024-01-17 02:30:03 -0400

    a

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -51,13 +51,10 @@ const LoginForm = () => {
       if (response.ok) {
         localStorage.setItem("user_role", responseData.user_role);
         localStorage.setItem("user_id", responseData.user_id);
-        localStorage.setItem("user_name", responseData.user_name);
-        localStorage.setItem("user_email", responseData.user_email);
 
         if (responseData.user_role === "admin") {
           window.location.href = "/admin/dashboard";
           console.log(responseData)
-        
         } else {
           console.log(responseData)
 
