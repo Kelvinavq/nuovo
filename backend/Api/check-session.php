c577256907c0d3270ffcdb8252658cb570b46aee
Kelvinavq <kvalera200244@gmail.com>
2024-01-16 22:05:43 -0400

    a

--- backend/Api/check-session.php
+++ backend/Api/check-session.php
@@ -9,5 +9,8 @@ session_start();
 if (isset($_SESSION['user_id'])) {
     http_response_code(200);
     echo json_encode(array("message" => "Sesión válida."));
-} 
+} else {
+    http_response_code(401); // Unauthorized
+    echo json_encode(array("error" => "Sesión no válida."));
+}
 ?>
