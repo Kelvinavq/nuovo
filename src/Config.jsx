a15efbb227770f8d1596b6e043c722415483f3ee
Kelvinavq <kvalera200244@gmail.com>
2024-02-15 01:39:19 -0400

    dependencias

--- src/Config.jsx
+++ src/Config.jsx
@@ -2,6 +2,7 @@
 const Config = {
     backendBaseUrl: "http://localhost/nuovo/backend/Api/",
     backendBaseUrlAdmin: "http://localhost/nuovo/backend/Api/admin/",
+    imgProfileUser: "http:",
   };
   
   export default Config;
